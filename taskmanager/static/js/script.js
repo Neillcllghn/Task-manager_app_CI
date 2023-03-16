f788b260e64d1546147977d3d6b978ae06f8a326
Neillcllghn <neillcllghn@outlook.com>
2023-03-16 22:14:08 +0000

    Adding Tasks

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -3,3 +3,14 @@ document.addEventListener('DOMContentLoaded', function() {
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
   });
+
+  // datepicker intialisation
+  let datepicker = document.querySelectorAll('.datepicker');
+  M.Datepicker.init(datepicker, {
+    format: "dd mmmm, yyyy",
+    i18n: {done: "Select"}
+  });
+
+  // select intialisation
+  let selects = document.querySelectorAll('select');
+  M.FormSelect.init(selects);
